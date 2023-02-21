5c128c65963806b52f207947ccc537baffc2667a
sergio estevao <smestevao11@gmail.com>
2023-02-21 18:26:51 +0000

    feat: results && refactored file structure

---
+++ src/lineproduct.cpp
@@ -0,0 +1,58 @@
+/*
+ *  lineproduct.cpp - Line based matrix multiplication
+ *  CPA @ M.EIC, 2023
+ *  Authors:
+ *      Miguel Rodrigues <up201906042@edu.fe.up.pt>
+ *      Sérgio Estêvão <up201905680@edu.fe.up.pt>
+ */
+#include <chrono>
+#include <memory>
+#include "mphelpers.h"
+
+
+// globals & aliases
+using matrix_t = double[];
+using matrix_size_t = std::size_t;
+static constexpr matrix_size_t N = 2000;
+
+
+std::unique_ptr<matrix_t>
+mult(const matrix_t a, const matrix_t b)
+{
+    auto result = std::make_unique<matrix_t>(N * N);
+
+    for (matrix_size_t i = 0; i < N; i++)
+        for (matrix_size_t k = 0; k < N; k++)
+            for (matrix_size_t j = 0; j < N; j++)
+                result[i * N + j] += a[i * N + k] * b[k * N + j];
+
+    return result;
+}
+
+
+int
+main(void)
+{
+    /* helper struct which handles PAPI internals */
+    cpa::matrix_product::PAPI_helper helper;
+
+    /* matrix allocation and filling */
+    auto a = std::make_unique<matrix_t>(N * N);
+    auto b = std::make_unique<matrix_t>(N * N);
+    cpa::matrix_product::fill(a.get(), b.get(), N);
+
+    helper.start();
+
+    /* perform the computation of matrix multiplication */
+    const auto start = std::chrono::steady_clock::now();
+    auto c = mult(a.get(), b.get());
+    const auto end = std::chrono::steady_clock::now();
+
+    helper.stop();
+
+    /* report the results */
+    const auto elapsed = end - start;
+    cpa::matrix_product::report(helper, elapsed, N);
+
+    return 0;
+}
---
+++ src/mphelpers.h
@@ -0,0 +1,124 @@
+/*
+ *  mphelpers.h - set of helpers for matrix-product
+ *  CPA @ M.EIC, 2023
+ *  Authors:
+ *      Miguel Rodrigues <up201906042@edu.fe.up.pt>
+ *      Sérgio Estêvão <up201905680@edu.fe.up.pt>
+ */
+#ifndef MPHELPERS_H
+#define MPHELPERS_H
+
+#include <array>
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <string_view>
+#include <papi.h>
+
+
+namespace cpa::matrix_product {
+
+using matrix_t = double[];
+using matrix_size_t = std::size_t;
+using duration_t = std::chrono::duration<double>;
+
+
+struct PAPI_helper {
+public:
+    PAPI_helper()
+    {
+        retval = PAPI_library_init(PAPI_VER_CURRENT);
+        if (retval != PAPI_VER_CURRENT && retval > 0)
+            handle_error("PAPI_library_init");
+
+        retval = PAPI_create_eventset(&eventset);
+        if (retval != PAPI_OK)
+            handle_error("PAPI_create_eventset");
+
+        retval = PAPI_add_events(eventset, event_codes.data(), event_codes.size());
+        if (retval != PAPI_OK)
+            handle_error("PAPI_add_events");
+    }
+
+    void start()
+    {
+        retval = PAPI_start(eventset);
+        if (retval != PAPI_OK)
+            handle_error("PAPI_start");
+    }
+
+    void stop()
+    {
+        retval = PAPI_stop(eventset, values.data());
+        if (retval != PAPI_OK)
+            handle_error("PAPI_stop");
+    }
+
+    void report() const
+    {
+        std::cout << "PAPI_L1_DCM: " << values[0] << '\n'
+                  << "PAPI_L2_DCM: " << values[1] << '\n'
+                  << "PAPI_TOT_INS: " << values[2] << '\n'
+                  << "PAPI_TOT_CYC: " << values[3] << std::endl;
+    }
+
+    ~PAPI_helper()
+    {
+        retval = PAPI_reset(eventset);
+        if (retval != PAPI_OK)
+            handle_error("PAPI_reset");
+
+        retval = PAPI_remove_events(eventset, event_codes.data(), event_codes.size());
+        if (retval != PAPI_OK)
+            handle_error("PAPI_remove_events");
+
+        retval = PAPI_destroy_eventset(&eventset);
+        if (retval != PAPI_OK)
+            handle_error("PAPI_destroy_eventeset");
+    }
+
+private:
+    void handle_error(std::string_view callable) const
+    {
+        std::cerr << callable << " failed with error "
+                  << retval << ' '
+                  << '(' << PAPI_strerror(retval) << ')' << std::endl;
+        std::exit(1);
+    }
+
+private:
+    std::array<int, 4> event_codes = { PAPI_L1_DCM, PAPI_L2_DCM, PAPI_TOT_INS, PAPI_TOT_CYC };
+    std::array<long long, 4> values = { 0, 0, 0, 0 };
+    int eventset = PAPI_NULL;
+    int retval = 0;
+};
+
+
+constexpr void
+fill(matrix_t a, matrix_t b, const matrix_size_t N)
+{
+    for (matrix_size_t i = 0; i < N; i++) {
+        for (matrix_size_t j = 0; j < N; j++) {
+            a[i * N + j] = 1.0;
+            b[i * N + j] = static_cast<double>(i + 1);
+        }
+    }
+}
+
+void
+report(const cpa::matrix_product::PAPI_helper& helper,
+       const duration_t& elapsed,
+       const matrix_size_t N)
+{
+    const auto flops = 2 * N * N * N;
+    const auto capacity = flops / elapsed.count();
+    std::cout << "size: " << N << '\n'
+              << "flops: " << flops << '\n'
+              << "duration: " << elapsed.count() << 's' << '\n'
+              << "capacity: " << capacity << std::endl;
+
+    helper.report();
+}
+}
+
+#endif /* MPHELPERS_H */
